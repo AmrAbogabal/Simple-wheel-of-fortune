93ef585c89d5fde9a4da0683423199aeddab7a5d
abogabal <amrabogabal77@gmail.com>
2023-06-29 14:25:36 +0300

    Amr Abo Gabal

--- lib/Function.dart
+++ lib/Function.dart
@@ -5,4 +5,3 @@ import 'package:flutter/cupertino.dart';
 Text Textstylee(Color c,String T , double x  ){
   return Text(T,style: TextStyle(color: c , fontSize:x,fontWeight: FontWeight.bold ),) ;
 }
-//Amr Abo Gabal
